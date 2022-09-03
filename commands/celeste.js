4936ce259f515402681ff71f05aed75cc6ac4f32
Ekpy <33184056+Ekpy@users.noreply.github.com>
2022-09-03 08:02:53 +0300

    Add files via upload

---
+++ commands/celeste.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require('discord.js');
+const request = require('request');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('celeste')
+		.setDescription('Gets the current price of Celeste'),
+	async execute(interaction) {
+
+        request('https://store.steampowered.com/api/appdetails?appids=504230', (error, response, body) => {
+            if (error) {console.error(error);}
+            else {
+                const object = JSON.parse(body);
+                const data = object['504230']['data']['price_overview'];
+                if (data['discount_percent'] != 0) {
+                    interaction.reply (`Celeste is currently ${data['final_formatted']}, a saving of ${data['discount_percent']}%`);
+                }
+                else {
+                    interaction.reply(`Celeste is currently ${data['final_formatted']} as normal`);
+                }
+            }
+        });
+	},
+};
+
+
+/*
+import requests
+req = requests.get(url="https://store.steampowered.com/api/appdetails?appids=504230")
+data = req.json().get('504230').get('data').get('price_overview')
+if data.get('discount_percent') != 0:
+    print(f"Celeste is currently {data.get('final_formatted')}, a saving of {data.get('discount_percent')}%")
+else:
+    print(f"Celeste is currently {data.get('final_formatted')} as normal")
+    */
