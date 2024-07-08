d964888dc82adab2d01fe18e7f974aaebe72c941
Ekpy <33184056+Ekpy@users.noreply.github.com>
2024-07-08 16:37:04 +0100

    View age of minecraft updates command. SFX for cricket I forgot (Merlin)

--- commands/help.js
+++ commands/help.js
@@ -73,10 +73,12 @@ module.exports = {
                 { name: '/ping', value: 'Says "Pong!" back.' },
                 { name: '/qoute', value: 'The qoute game! Finish the qoute, and dont gain any points' },
                 { name: '/oneshot', value: 'Get the price of the hit game :yellow_heart: Oneshot :yellow_heart: . You should fucking buy it play it. Who does gets Meros Premium' },
+                { name: '/howoldisminecraft', value: 'Input a valid minecraft update, to find out how old it is' },
                 { name: '/cricket', value: 'Joins the channel, to play a cricket sound. For when a joke just does not land.' },
                 { name: '/chair', value: 'A nice chair' },
                 { name: '\u200B', value: '\u200B' },
                 { name: 'Context Menu', value: 'You get to these commands by right clicking a person and picking Apps' },
+                { name: 'Get avatar', value: 'Get the avatar of the person' },
                 { name: 'Update Qoute game', value: 'Updates the qoute game. Qoutes are split by the first `-` that appears in a message. So please fix any if you got any mistakes ' },
                 { name: '\u200B', value: '\u200B' },
             )
